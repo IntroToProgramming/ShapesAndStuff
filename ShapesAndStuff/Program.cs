f6b910e35eff192e79bfde4e6e67983ea865e91b
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-18 16:26:06 -0400

    Add project files.

---
+++ ShapesAndStuff/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShapesAndStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var shapes = new List<Shape>
+            {
+                new Square(),
+                new Rectangle(),
+                new Square(),
+                new Ellipse(),
+                new Circle()
+            };
+
+
+            var utils = new ShapeUtils();
+            foreach (var shape in shapes)
+            {
+                utils.ScaleShapesIfTheyCan(shape, 1.20F);
+            }
+        }
+    }
+
+    public class ShapeUtils
+    {
+
+        public object ObjectFactory(string description)
+        {
+            switch(description)
+            {
+                case "square":
+                    return new Square();
+                case "circle":
+                    return new Circle();
+                case var x when (x?.Trim().Length ?? 0) == 0:
+                    return new Ellipse();
+                default:
+                    return "No idea. You are on your own";
+            }
+        }
+
+
+        public void DoSomethingWithSquared(Square sq)
+        {
+            switch (sq)
+            {
+                case Square s when s.Height > 20:
+                    Console.WriteLine("That square is tall!");
+                    break;
+                case Square s when s.Height < 5 && s.Width < 5:
+                    Console.WriteLine("That is a teeny square!");
+                    break;
+                case null:
+                    Console.WriteLine("Don't Give That!");
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        public void ScaleShapesIfTheyCan(Shape s, float p)
+        {
+            switch (s)
+            {
+                case ICanScaleHorizontally sh:
+                    sh.ScalePercentage(p);
+                    return;
+                case ICanScaleVertically sh:
+                    sh.ScalePercentage(p);
+                    return;
+                case ICanScale sh:
+                    sh.ScalePercentage(p);
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        public void DoSomething(Shape s)
+        {
+            Console.WriteLine(s.Description);
+            if (s is ICanScaleHorizontally)
+            {
+                ICanScaleHorizontally ptr = (ICanScaleHorizontally)s;
+                ptr.ScalePercentage(1.1F);
+
+                //((ICanScaleHorizontally)s).ScalePercentage(1.1F);
+            }
+
+            var sh = s as ICanScaleHorizontally;
+            if (sh != null)
+            {
+                // sh is a hugger/
+            }
+
+
+        }
+    }
+}
---
+++ ShapesAndStuff/Shape.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesAndStuff
+{
+    public interface ICanScaleHorizontally
+    {
+        public void ScalePercentage(float percentage);
+    }
+    public interface ICanScaleVertically
+    {
+        public void ScalePercentage(float percentage);
+    }
+    public interface ICanScale
+    {
+        public void ScalePercentage(float percentage);
+    }
+    public abstract class Shape
+    {
+        public string Description { get; protected set; } = "No Description set";
+
+    }
+
+    public class Square : Shape, ICanScale
+    {
+        public Square()
+        {
+            Description = "I am a square!";
+        }
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        public void ScalePercentage(float percentage)
+        {
+            Console.WriteLine("Scaling the square in ALL directions");
+        }
+    }
+    public class Rectangle : Shape, ICanScale, ICanScaleHorizontally, ICanScaleVertically
+    {
+        public Rectangle()
+        {
+            Description = "I'm a rectangle!";
+        }
+
+        void ICanScale.ScalePercentage(float percentage)
+        {
+            Console.WriteLine("Scaling the Rectagle In All Directions");
+        }
+
+        void ICanScaleHorizontally.ScalePercentage(float percentage)
+        {
+            Console.WriteLine("Scaling the Rectagle Horizontally");
+        }
+
+        void ICanScaleVertically.ScalePercentage(float percentage)
+        {
+            Console.WriteLine("Scaling the Rectagle Vertically");
+        }
+    }
+    public class Circle : Shape
+    {
+    }
+    public class Ellipse : Shape
+    {
+
+    }
+}
